96dcb583180cf523fe42ed8774a92ce39744c54a
HeisenbergX98 <ayrtonmiap001@gmail.com>
2025-10-22 16:38:19 +0200

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const repoName = 'wianki-gosi';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/wianki-gosi/`,
   plugins: [react()],
-  base: `/${repoName}/`,
-})
+});
